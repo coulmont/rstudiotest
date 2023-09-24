acd333f8313b0ea9fac723cdfd66024c6b052e58
coulmont <coulmont@yahoo.com>
2023-09-24 14:21:24 +0200

    modifications

--- code/main_code.R
+++ code/main_code.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-# 
+
 df <- read_csv("data/GLB.Ts+dSST.csv",skip = 1)
 
 temperatures <- df %>%
@@ -9,15 +9,27 @@ temperatures <- df %>%
   filter(!is.na(diff_temp))
 
 
-temperatures %>%
+temperatures <- temperatures %>%
   mutate(date = ymd(paste(Year,month,1))) %>%
   group_by(Year) %>%
-  mutate(color_temp = mean(diff_temp)) %>%
+  mutate(color_temp = mean(diff_temp))
+  
+
+temperatures %>%
   ggplot(aes(date,diff_temp)) +
   geom_point(aes(color=color_temp),size=1) +
   scale_color_distiller(palette="RdYlBu") +
   theme(panel.background = element_rect(fill="#555555"),
         panel.grid = element_line(color="#999999"))
+
+
+temperatures %>%
+  mutate(date_group = ymd(paste("2000",month,1))) %>%
+  ggplot(aes(date_group, diff_temp, group = Year)) +
+  geom_line(aes(color=color_temp)) +
+  coord_polar() +
+  scale_y_continuous(limits=c(-2,2))
+
 
 temperatures %>%
   ggplot(aes())
